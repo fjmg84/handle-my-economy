0f0390d76b443e9728ecfcec1e3f609174374834
fjmg84 <fjmgqba@gmail.com>
2026-04-13 17:32:09 -0500

    feat: implement pagination for recent transactions in FinanceRecently component

--- components/lists/FinanceRecently.tsx
+++ components/lists/FinanceRecently.tsx
@@ -1,47 +1,66 @@
-'use client';
+"use client";
 
-import useFinanceHook from '@/stores/useFinanceHook';
-import { Trash2 } from 'lucide-react';
-
+import usePagination from "@/hooks/usePagination";
+import useFinanceHook from "@/stores/useFinanceHook";
+import { Trash2 } from "lucide-react";
 
 export default function FinanceRecently() {
- const { transactions, removeTransaction } = useFinanceHook();
- 
+
+  const { transactions, removeTransaction } = useFinanceHook();
+  const {Component: PaginationComponent, currentPageTransactions } = usePagination(transactions);
 
   return (
     <div>
       <div className="mb-6">
-        <h2 className="text-xl font-bold text-gray-900">Transacciones Recientes</h2>
+        <h2 className="text-xl font-bold text-gray-900">
+          Transacciones Recientes
+        </h2>
         <p className="text-sm text-gray-600 mt-1">
           Transacciones de tus ingresos y gastos
         </p>
       </div>
-      
-      <div className="mt-6 space-y-4 h-full overflow-y-auto max-h-120">
-        
-        {transactions.slice(-10).map((t) => (
-          <div key={t.id} className="bg-gray-50 p-4 rounded-lg flex justify-between items-center mr-4">
+
+      <div className="mt-6 space-y-4 overflow-y-auto h-120">
+        {currentPageTransactions.map((t) => (
+          <div
+            key={t.id}
+            className="bg-gray-50 p-4 rounded-lg flex justify-between items-center mr-4"
+          >
             <div>
               <p className="font-semibold">{t.description}</p>
               <p className="text-sm text-gray-600">{t.category}</p>
+              <p className="text-xs text-gray-600">{new Date(t.date).toLocaleDateString()}</p>
+              
             </div>
             <div className="text-right flex items-center">
-              <span className={t.type === 'income' ? 'text-green-600' : 'text-red-600'}>
-                {t.type === 'income' ? '+' : '-'}$ {t.amount.toLocaleString()}
+              <span
+                className={
+                  t.type === "income" ? "text-green-600" : "text-red-600"
+                }
+              >
+                {t.type === "income" ? "+" : "-"}$ {t.amount.toLocaleString()}
               </span>
-              
-                  <button
-                  onClick={() => removeTransaction(t.id || '')}
-                  className="ml-4 text-red-600 hover:text-red-800 hover:bg-red-100 p-1 rounded hover:cursor-pointer transition"
-                  >
+
+              <button
+                onClick={() => removeTransaction(t.id || "")}
+                className="ml-4 text-red-600 hover:text-red-800 hover:bg-red-100 p-1 rounded hover:cursor-pointer transition"
+              >
                 <Trash2 size={20} />
               </button>
-            
-
             </div>
           </div>
         ))}
+
+
+      
       </div>
+        {transactions.length === 0 && (
+          <p className="text-center text-gray-500">
+            No hay transacciones registradas
+          </p>
+        )}
+
+        <PaginationComponent />
     </div>
   );
 }
---
+++ hooks/usePagination.tsx
@@ -0,0 +1,77 @@
+import { Transaction } from "@/types/finance";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useState } from "react";
+
+function usePagination(transactions: Transaction[]) {
+  let rowPerPage = 5;
+  let maxPage = Math.ceil(transactions.length / rowPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const getCurrentPageTransactions = () => {
+    const startIndex = (currentPage - 1) * rowPerPage;
+
+
+    const orderedTransactions = transactions.sort((a, b) => {
+        const dateA = new Date(a.createdAt || a.date).getTime();
+        const dateB = new Date(b.createdAt || b.date).getTime();
+        return dateB - dateA; // Orden descendente
+    });
+
+    return orderedTransactions.slice(startIndex, startIndex + rowPerPage);
+  };
+
+  const goToNextPage = () => {
+    if (currentPage < maxPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const Component = () => {
+
+    const disabledNext = currentPage >= maxPage;
+    const disabledPrevious = currentPage <= 1;
+
+    return (
+      <div id="paginations">
+        <div className="flex items-center justify-center">
+          <button
+            className={`p-1 rounded transition ml-2 hover:cursor-pointer hover:disabled:cursor-not-allowed hover:disabled:bg-transparent`}
+            onClick={goToPreviousPage}
+            disabled={disabledPrevious}
+          >
+            <ArrowLeft color={ disabledPrevious ? "#a1a1aa" : "#4f46e5"} size={20}/>
+          </button>
+          <button
+            className={`p-1 rounded transition ml-2 hover:cursor-pointer hover:disabled:cursor-not-allowed hover:disabled:bg-transparent`}
+            onClick={goToNextPage}
+            disabled={disabledNext}
+          >
+            <ArrowRight color={ disabledNext ? "#a1a1aa" : "#4f46e5"}  size={20}/>
+          </button>
+        </div>
+
+        <p className="text-center text-sm text-gray-600 mt-2">
+          Página {currentPage} de {maxPage} transacciones totales:{" "}
+          {transactions.length}
+        </p>
+      </div>
+    );
+  };
+
+  return {
+    Component,
+    currentPageTransactions: getCurrentPageTransactions(),
+    goToNextPage,
+    goToPreviousPage,
+    currentPage,
+    maxPage,
+  };
+}
+
+export default usePagination;
