5953948309afc51d89794343cb18a6d041171168
fjmg84 <fjmgqba@gmail.com>
2026-04-13 23:02:46 -0500

    feat: add categories and months constants for better data management

---
+++ utils/constant.ts
@@ -0,0 +1,25 @@
+export const categories = [
+  "Salario",
+  "Alquiler",
+  "Comida",
+  "Transporte",
+  "Entretenimiento",
+  "Salud",
+  "Educación",
+  "Otros",
+];
+
+export const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
