03800d636d637ff0753358d6f17cd47b6554b465
fjmg84 <fjmgqba@gmail.com>
2026-04-15 19:22:37 -0500

    feat: refactor TransactionsDisplay component for improved layout and styling

--- components/shares/TransactionsDisplay.tsx
+++ components/shares/TransactionsDisplay.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import useFinanceHook from "@/stores/useFinanceHook";
-import { ArrowUpDown, BanknoteArrowDown, BanknoteArrowUp } from "lucide-react";
+import { BanknoteArrowDown, BanknoteArrowUp } from "lucide-react";
 
 function TransactionsDisplay() {
   const { incomes, expenses } = useFinanceHook();
@@ -27,53 +27,44 @@ function TransactionsDisplay() {
   });
 
   const netTotal = incomeTotal - expenseTotal;
-  const colorNetTotal = netTotal >= 0 ? "text-green-800" : "text-red-800";
-  const bgColorNetTotal = netTotal >= 0 ? "bg-green-100" : "bg-red-100";
-  const iconColorNetTotal = netTotal >= 0 ? "#016630" : "#9f0712";
 
   return (
-    <div className="flex flex-col gap-4">
-      <h2 className="text-xs bg-lime-300 px-4 py-2 rounded-full w-max uppercase">
-        Transacciones del año actual
-      </h2>
+    <div className="flex flex-col gap-4 from-indigo-800 to-slate-900 bg-linear-to-br rounded-2xl p-6">
+      <h2 className="text-2xl text-white">Transacciones del año actual</h2>
+      <span className="text-base text-gray-300">
+        Resumen de ingresos, gastos y total neto
+      </span>
 
-      <div className="space-y-4 flex flex-col md:flex-row md:space-x-4 md:space-y-0 justify-between">
-        <div className="flex gap-4">
-          <div className="p-4 bg-green-100 rounded-lg">
-            <p className="text-sm text-green-800">Ingresos</p>
-            <div className="flex justify-center gap-4 items-center">
-              <BanknoteArrowUp color="#16a34a" size={20} />
+      <div className="flex flex-col md:flex-row gap-4 justify-start">
+        <div className="px-4 py-2 h-12.5 bg-green-100 rounded-3xl flex justify-center gap-4 items-center w-max">
+          <BanknoteArrowUp color="#16a34a" size={20} />
 
-              <p className="text-lg font-semibold text-green-800">
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                }).format(incomeTotal)}
-              </p>
-            </div>
-          </div>
-
-          <div className="p-4 bg-red-100 rounded-lg">
-            <p className="text-sm text-red-800">Gastos</p>
-            <div className="flex justify-center gap-4 items-center">
-              <BanknoteArrowDown color="#9f0712" size={20} />
-
-              <p className="text-lg font-semibold text-red-800">
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                }).format(expenseTotal)}
-              </p>
-            </div>
-          </div>
+          <p className="text-lg font-semibold text-green-800">
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            }).format(incomeTotal)}
+          </p>
         </div>
 
-        <div className={`p-4 ${bgColorNetTotal} rounded-lg`}>
-          <p className={`text-sm ${colorNetTotal}`}>Total Neto</p>
+        <div className="px-4 py-2 h-12.5 bg-red-100 rounded-3xl flex justify-center gap-4 items-center w-max">
+          <BanknoteArrowDown color="#9f0712" size={20} />
+
+          <p className="text-lg font-semibold text-red-800">
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            }).format(expenseTotal)}
+          </p>
+        </div>
+
+        <div
+          className={`px-4 py-2 h-12.5 bg-lime-300 rounded-3xl flex justify-center gap-4 items-center w-max`}
+        >
+          <p className={`text-sm text-lime-800`}>Total Neto</p>
 
           <div className="flex justify-center gap-4 items-center">
-            <ArrowUpDown color={iconColorNetTotal} size={20} />
-            <p className={`text-lg font-semibold ${colorNetTotal}`}>
+            <p className={`text-lg font-semibold text-lime-800`}>
               {new Intl.NumberFormat("en-US", {
                 style: "currency",
                 currency: "USD",
@@ -82,8 +73,6 @@ function TransactionsDisplay() {
           </div>
         </div>
       </div>
-
-      <div className=" p-0.5 bg-blue-100 rounded-full" />
     </div>
   );
 }
